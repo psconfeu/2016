93410110fad2341f6c357d6f54cfdb25ce04a74d
Øyvind Kallstad <oyvind.kallstad@crayon.com>
2016-04-26 10:23:14 +0200

    Added slides and code examples

--- README.md
+++ README.md
@@ -13,3 +13,6 @@
 | Get-Contributing | Simon Wahlin | [Slides](https://github.com/psconfeu/2016/raw/master/Simon%20Wahlin/Get-Contributing/Get-Contributing.pptx) |  |
 | PowerShell and KeePass | Christian Lehrer | [Slides](https://github.com/psconfeu/2016/blob/master/Christian%20Lehrer/PowerShell%2BKeePass/Slides-KeePass%2BPowerShell.zip) | [Demo Scripts](https://github.com/psconfeu/2016/blob/master/Christian%20Lehrer/PowerShell%2BKeePass/Scripts-KeePass%2BPowerShell.zip)  |
 | 802.1x and PowerShell | Christian Lehrer | [Slides](https://github.com/psconfeu/2016/blob/master/Christian%20Lehrer/802.1X/SystemCenterPowerShellandNAC.pptx)|  |
+| A practical example of how to approach (and deliver) a large and complex script project | Øyvind Kallstad | ? | ?
+| Chasing the seconds: Optimizing file enumeration by diving deep into the Windows API | Øyvind Kallstad | ? | ?
+| Regular Expressions and PowerShell | Øyvind Kallstad | ? | ?
---
+++ Øyvind Kallstad/ScriptProject/Move-HomeArea.ps1
@@ -0,0 +1,160 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()]
+    [Alias('Path')]
+    [string] $InputFile = 'C:\Demo\ScriptProject\inputfile_test.csv',
+
+    [Parameter()]
+    [int] $NumberOfPasses = 1,
+
+    [Parameter()]
+    [int] $DelayBetweenPasses = 0 # in seconds
+)
+
+function Get-Info {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $Username,
+
+        [Parameter()]
+        [string] $Path
+    )
+}
+
+function Invoke-SanityCheck {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [psobject] $InputObject
+    )
+
+    return $true
+}
+
+function Get-UserConnection {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $Username,
+
+        [Parameter()]
+        [string] $Hostname
+    )
+
+    return $true
+}
+
+function Invoke-RobocopyWrapper {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $Path,
+
+        [Parameter()]
+        [string] $Destination,
+
+        [Parameter()]
+        [string] $Username
+    )
+
+    return $false
+}
+
+function Update-ADUser {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $Username,
+
+        [Parameter()]
+        [hashtable] $PropertyValue
+    )
+
+    return $false
+}
+
+function Invoke-ProcessInput {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $Username,
+
+        [Parameter()]
+        [string] $Path
+    )
+
+    $inputInfo = Get-Info
+
+    # perform sanity check before we begin
+    if (Invoke-SanityCheck -InputObject $inputInfo) {
+        Write-Host 'Sanity check ok'
+
+        # check if user have open files on the server
+        if (-not (Get-UserConnection -Username $Username -Hostname $inputInfo.OldHomePathServer)) {
+            Write-Host 'No user connection found'
+
+            # perform copy of files
+            if (Invoke-RobocopyWrapper -Path $inputInfo.OldHomepath -Destination $inputInfo.NewHomePath -Username $Username) {
+                Write-Host 'Files copied'
+
+                # update the user object in AD
+                if (Update-ADUser -Username $Username -PropertyValue @{'homeDrive' = 'H:';'homeDirectory' = $inputInfo.newHomePathFull}) {
+                    Write-Host 'Updated ADUser'
+                }
+                else {
+                    # AD Update failed
+                    Write-Warning 'AD Update failed'
+                }
+            }
+            else {
+                # file copy failed
+                Write-Warning 'File copy failed'
+            }
+        }
+        else {
+            # user have open files
+            Write-Host 'User have open files / User connection found'
+            $script:SkippedBecauseOfOpenFiles += (,([PSCustomObject] @{Username = $inputInfo.Username;NewHomeShare = $inputInfo.NewHomePath}))
+            
+            # perform copy job anyway
+            if (Invoke-RobocopyWrapper -Path $inputInfo.OldHomepath -Destination $inputInfo.NewHomePath -Username $Username) {
+                # copy files
+                Write-Host 'Copying files anyway...'
+            }
+            else {
+                # copy failed
+                Write-Warning 'File Copy failed'
+            }
+        }
+    }
+    else {
+        # sanity check failed
+        Write-Warning 'Sanity check failed'
+    }
+}
+
+$currentPass = 0
+$processingArray = @()
+
+$processingArray = Import-Csv -Path $InputFile
+
+do {
+    $currentPass++
+    Write-Host "Current pass: $currentPass"
+    $script:SkippedBecauseOfOpenFiles = @()
+
+    foreach ($input in $processingArray) {
+        Invoke-ProcessInput -Username $input.Username -Path $input.NewHomeShare
+    }
+
+    $processingArray = $script:SkippedBecauseOfOpenFiles
+
+    if ($script:SkippedBecauseOfOpenFiles.Count -gt 0) {
+        if ($DelayBetweenPasses -gt 0) {
+            Write-Host "Delaying $DelayBetweenPasses seconds"
+            Start-Sleep -Seconds $DelayBetweenPasses
+        }
+    }
+} until (($currentPass -eq $NumberOfPasses) -or ($SkippedBecauseOfOpenFiles.Count -eq 0))
+
